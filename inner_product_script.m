b322366e73c4807f16ca9895d753c539bdf2bd8c
takekimu <mucun.wuxian@gmail.com>
2016-05-22 23:53:28 +0900

    adjust ...

--- inner_product_script.m
+++ inner_product_script.m
@@ -14,21 +14,21 @@ for calc_i = 1:calc_num
     y_matlab = mtimes(w', x);
 end
 time = toc;
-fprintf('vector’·:%d, matlab        ‚Å‚Ì“àÏŒvZŠÔ:%f\n', size(w, 1), time);
+fprintf('vector’·:%d, matlab        ‚Å‚Ì“àÏŒvZŠÔ:%.2f\n', size(w, 1), time);
 
 tic;
 for calc_i = 1:calc_num
     y_mex = inner_product_mex(w, x);
 end
 time = toc;
-fprintf('vector’·:%d, mex           ‚Å‚Ì“àÏŒvZŠÔ:%f\n', size(w, 1), time);
+fprintf('vector’·:%d, mex           ‚Å‚Ì“àÏŒvZŠÔ:%.2f\n', size(w, 1), time);
 
 tic;
 for calc_i = 1:calc_num
     y_mex_omp = inner_product_omp_mex(w, x);
 end
 time = toc;
-fprintf('vector’·:%d, mex+openmp    ‚Å‚Ì“àÏŒvZŠÔ:%f\n', size(w, 1), time);
+fprintf('vector’·:%d, mex+openmp    ‚Å‚Ì“àÏŒvZŠÔ:%.2f\n', size(w, 1), time);
 
 tic;
 w_s = single(w);
@@ -37,7 +37,7 @@ for calc_i = 1:calc_num
     y_mex_omp_sse = inner_product_omp_sse_mex(w_s, x_s);
 end
 time = toc;
-fprintf('vector’·:%d, mex+openmp+sse‚Å‚Ì“àÏŒvZŠÔ:%f\n', size(w, 1), time);
+fprintf('vector’·:%d, mex+openmp+sse‚Å‚Ì“àÏŒvZŠÔ:%.2f\n', size(w, 1), time);
 
 tic;
 w_s = single(w);
@@ -46,7 +46,7 @@ for calc_i = 1:calc_num
     y_mex_omp_avx = inner_product_omp_avx_mex(w_s, x_s);
 end
 time = toc;
-fprintf('vector’·:%d, mex+openmp+avx‚Å‚Ì“àÏŒvZŠÔ:%f\n', size(w, 1), time);
+fprintf('vector’·:%d, mex+openmp+avx‚Å‚Ì“àÏŒvZŠÔ:%.2f\n', size(w, 1), time);
 
 tic;
 w_s = gpuArray(single(w));
